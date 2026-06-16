20561463610ab0e0d27a6bf4fd8c2a045e23cd4d
abdelhamid777 <abdelhamid7557@gmail.com>
2026-06-16 12:58:31 +0300

    Update SettingsMenu.js

--- blob/main/src/components/SettingsMenu.js
+++ blob/main/src/components/SettingsMenu.js
@@ -1,1 +1,12 @@
+import React from 'react';
+import { View, Button } from 'react-native';
 
+export default function SettingsMenu() {
+  return (
+    <View>
+      <Button title="Settings" />
+      <Button title="Notifications" />
+      <Button title="About" />
+    </View>
+  );
+}

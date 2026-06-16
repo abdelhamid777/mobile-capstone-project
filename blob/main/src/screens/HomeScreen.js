abd7d52e1825944acf9bb492fbfee1a785a4af9a
abdelhamid777 <abdelhamid7557@gmail.com>
2026-06-16 12:55:52 +0300

    Update HomeScreen.js

--- blob/main/src/screens/HomeScreen.js
+++ blob/main/src/screens/HomeScreen.js
@@ -1,1 +1,24 @@
+import React from 'react';
+import { View, Text, FlatList } from 'react-native';
 
+const data = [
+  { id: '1', title: 'Item One' },
+  { id: '2', title: 'Item Two' },
+  { id: '3', title: 'Item Three' }
+];
+
+export default function HomeScreen() {
+  return (
+    <View>
+      <Text>Application Home</Text>
+
+      <FlatList
+        data={data}
+        keyExtractor={(item) => item.id}
+        renderItem={({ item }) => (
+          <Text>{item.title}</Text>
+        )}
+      />
+    </View>
+  );
+}

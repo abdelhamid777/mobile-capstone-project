588fe5162cecc88188e3c90a9173e878057ce07e
abdelhamid777 <abdelhamid7557@gmail.com>
2026-06-16 12:56:39 +0300

    Update AsyncStorageService.js

--- blob/main/src/storage/AsyncStorageService.js
+++ blob/main/src/storage/AsyncStorageService.js
@@ -1,1 +1,18 @@
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
+export const saveData = async (key, value) => {
+  try {
+    await AsyncStorage.setItem(key, JSON.stringify(value));
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const getData = async (key) => {
+  try {
+    const value = await AsyncStorage.getItem(key);
+    return value ? JSON.parse(value) : null;
+  } catch (error) {
+    console.log(error);
+  }
+};

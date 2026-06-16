be851de9103297c57a31ed2163417fa813d02edd
abdelhamid777 <abdelhamid7557@gmail.com>
2026-06-16 12:56:13 +0300

    Update DetailScreen.js

--- blob/main/src/screens/DetailScreen.js
+++ blob/main/src/screens/DetailScreen.js
@@ -1,1 +1,12 @@
+import React from 'react';
+import { View, Text } from 'react-native';
 
+export default function DetailScreen() {
+  return (
+    <View>
+      <Text>Item Details</Text>
+      <Text>Description of selected item.</Text>
+      <Text>Category Information</Text>
+    </View>
+  );
+}

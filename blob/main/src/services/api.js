667b0a30c11bd2a003436feb5cfc025f7dceb219
abdelhamid777 <abdelhamid7557@gmail.com>
2026-06-16 12:57:10 +0300

    Update api.js

--- blob/main/src/services/api.js
+++ blob/main/src/services/api.js
@@ -1,1 +1,12 @@
+const API_URL = 'https://jsonplaceholder.typicode.com/posts';
 
+export const fetchData = async () => {
+  try {
+    const response = await fetch(API_URL);
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.log(error);
+    return [];
+  }
+};
